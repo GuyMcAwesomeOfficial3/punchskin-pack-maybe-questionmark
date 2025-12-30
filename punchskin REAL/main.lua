896df308cef7a6c0bf6ee5995f5640e24c273c85
GuyMcAwesomeOfficial3 <imnotfeelingverychillrightnow@gmail.com>
2025-12-29 23:23:07 -0500

    Add files via upload
    this is the new one
    :)

--- punchskin REAL/main.lua
+++ punchskin REAL/main.lua
@@ -15,10 +15,47 @@ AltTexture({ -- my jokers :D
         name = 'Jelkers'
     }
 })
+AltTexture({ -- my decks :D
+    key = 'pp_decks',
+    set = 'Back',
+    path = 'pp_decks.png',
+    loc_txt = {
+        name = 'Dicks'
+    }
+})
+AltTexture({ -- my tarots :D
+    key = 'pp_tarot',
+    set = 'Tarot',
+    path = 'pp_tarots.png',
+    loc_txt = {
+        name = 'Tarts'
+    }
+})
+AltTexture({ -- my planets :D
+    key = 'pp_planet',
+    set = 'Planet',
+    path = 'pp_tarots.png',
+    loc_txt = {
+        name = 'Plants'
+    }
+})
+AltTexture({ -- my spectrals :D
+    key = 'pp_spectral',
+    set = 'Spectral',
+    path = 'pp_tarots.png',
+	soul = 'pp_decks.png',
+    loc_txt = {
+        name = 'Spectrals'
+    }
+})
 TexturePack{ -- Punchskin Pack
     key = 'pp',
     textures = {
         'pptextures_pp_jokers',
+		'pptextures_pp_tarot',
+		'pptextures_pp_planet',
+		'pptextures_pp_spectral',
+		'pptextures_pp_decks',
     },
     loc_txt = {
         name = 'Punchskin Pack',
